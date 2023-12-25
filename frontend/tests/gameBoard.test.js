7b1473f37e0078e270535c49f959ab7168eb4d40
szymon.kisiela <szymon.kisiela@asseco.pl>
2023-12-25 03:36:43 +0100

    Add new unit test file for gameBoard module.

--- frontend/tests/gameBoard.test.js
+++ frontend/tests/gameBoard.test.js
@@ -1,31 +1,34 @@
-const { test, expect } = require('@playwright/test');
+const {test, expect} = require('@playwright/test');
 
 test.describe('GameBoard Component Tests', () => {
-    test.beforeEach(async ({ page }) => {
+    test.beforeEach(async ({page}) => {
         await page.goto('http://localhost:8080/'); // Adres URL Twojego projektu Vue.js
     });
 
-    test('Should display initial game state', async ({ page }) => {
+    test('Should display initial game state', async ({page}) => {
         await expect(page.locator('h1')).toContainText(''); // Początkowy stan wiadomości
         await expect(page.locator('h2')).toContainText('Aktualny gracz: '); // Początkowy stan aktualnego gracza
         const cells = page.locator('.cell');
         await expect(cells).toHaveCount(9); // Sprawdź, czy są 3x3 komórki
     });
 
-    test('Should start new game on button click', async ({ page }) => {
+    test('Should start new game on button click', async ({page}) => {
         await page.click('button:has-text("Start New Game")');
         await expect(page.locator('h2')).toContainText('Aktualny gracz: X'); // Załóżmy, że X zaczyna grę
     });
 
-    test('Should make a move and update the board', async ({ page }) => {
+    test('Should make a move and update the board', async ({page}) => {
         await page.click('button:has-text("Start New Game")');
-        // Wybierz pierwszą pustą komórkę i kliknij
-        await page.click('.cell:has-text("")');
-        // Sprawdź, czy pierwszy ruch został wykonany
-        await expect(page.locator('.cell')).toContainText('X');
+        // Click on the first cell in the top row
+        const firstCell = page.locator('.cell').nth(0);
+        await firstCell.click();
+        // Po kliknięciu sprawdź, czy komórka jest zablokowana (disabled) i zawiera 'X'
+        await expect(firstCell).toHaveAttribute('disabled');
+        await expect(firstCell).toContainText('X');
     });
 
-    test('Should reset the game', async ({ page }) => {
+
+    test('Should reset the game', async ({page}) => {
         await page.click('button:has-text("Start New Game")');
         await page.click('.cell:has-text("")');
         await page.click('button:has-text("Reset Game")');
@@ -35,4 +38,5 @@ test.describe('GameBoard Component Tests', () => {
             await expect(cells.nth(i)).toContainText('');
         }
     });
+
 });
