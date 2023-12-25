153fcbb136e3f6cb5b5bac3026b8d7301463131f
szymon.kisiela <szymon.kisiela@asseco.pl>
2023-12-25 16:18:03 +0100

    Delete api.js, update app.py,

---
+++ frontend/vue.config.js
@@ -0,0 +1,13 @@
+const { defineConfig } = require('@vue/cli-service');
+module.exports = defineConfig({
+  transpileDependencies: true,
+  devServer: {
+    proxy: {
+      "/api": {
+        target: "http://10.0.1.2:8080", // Poprawny adres URL
+        changeOrigin: true,
+        pathRewrite: { "^/api": "" }
+      }
+    }
+  }
+});
